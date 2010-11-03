6011048f8a93862840fc306ce8446913bcb7d8d9
Xiao Peng <eagle.xiao@gmail.com>
2010-11-03 22:47:26 +0800

    added ant junit

--- test/com/ixpense/IXpenseServletTest.java
+++ test/com/ixpense/IXpenseServletTest.java
@@ -9,6 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -26,7 +29,7 @@ public class IXpenseServletTest {
     PrintWriter printWriter;
 
     @Before
-    public void setUp(){
+    public void setUp() {
         initMocks(this);
     }
 
@@ -40,5 +43,14 @@ public class IXpenseServletTest {
 
         verify(httpServletResponse).setContentType("text/plain");
 
+    }
+
+    @Test
+    public void integration_test_to_get_data_from_your_expense() throws IOException {
+        URL url = new URL("http://www.example.com/comment");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        
+//        connection.setRequestProperty("Authorization",
+//                "Basic " + codec.encodeBase64String(("username:password").getBytes());
     }
 }
